0840ce8d222a9839a3c43dbdcfa6cc300449d64e
Mykhailo Kukharskyi <mykhailo.kukhraskyi@right.link>
2024-02-19 01:41:17 +0200

    Add default layout. Config sass styles. Add pages directory and index page

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="sass">
+.container
+  max-width: 1440px
+  margin: 0 auto
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        sass: {
+          additionalData: '@use "@/assets/style/_main.sass"',
+        },
+      },
+    },
+  }
 })
